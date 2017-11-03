0a1f154d309975064aa7b6714d3c3396879e0703
Matthew Daigle <matthewjdaigle@users.noreply.github.com>
2017-11-02 21:46:50 -0700

    Updating main and package for icons.

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, './app/images/png/64x64.png')
+    })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
